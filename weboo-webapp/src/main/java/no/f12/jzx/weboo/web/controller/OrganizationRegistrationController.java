a14e5650f0ddfb5695e04dd30978ff178d770b42
Anders Sveen <anders@f12.no>
2011-05-21 10:51:05 +0200

    Just small change to import to clarify code

--- weboo-webapp/src/main/java/no/f12/jzx/weboo/web/controller/OrganizationRegistrationController.java
+++ weboo-webapp/src/main/java/no/f12/jzx/weboo/web/controller/OrganizationRegistrationController.java
@@ -12,27 +12,29 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
+import static no.f12.jzx.weboo.web.controller.NavigationRegistry.*;
+
 @Controller
-@RequestMapping(value = "/" + NavigationRegistry.URL_ORGANIZATION)
+@RequestMapping(value = "/" + URL_ORGANIZATION)
 public class OrganizationRegistrationController {
 
 	@Autowired
 	OrganizationRepository orgRepo;
 
 	@RequestMapping(method = RequestMethod.GET)
-	public String showNewInput(@ModelAttribute Organization organization) {
-		return NavigationRegistry.VIEW_ORGANIZATION_FORM;
+	public String showOrganizationInput(@ModelAttribute Organization organization) {
+		return VIEW_ORGANIZATION_FORM;
 	}
 
 	@RequestMapping(method = RequestMethod.POST)
 	public String registerNewOrganization(@Valid @ModelAttribute Organization organization, Errors errors) {
 		if (errors.hasErrors()) {
-			return NavigationRegistry.VIEW_ORGANIZATION_FORM;
+			return VIEW_ORGANIZATION_FORM;
 		}
 
 		this.orgRepo.addOrganization(organization);
 
-		return NavigationRegistry.VIEW_ORGANIZATION_SUMMARY;
+		return VIEW_ORGANIZATION_SUMMARY;
 	}
 
 	public void setOrganizationRepository(OrganizationRepository orgRepo) {
