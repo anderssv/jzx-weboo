04fbd2cd32568b3c5bbbb483727be80e9c7f48f6
Anders Sveen <anders@f12.no>
2011-09-08 23:18:49 +0200

    Refactored all into one server class

--- weboo-webapp/src/main/java/no/f12/jzx/weboo/server/WebServer.java
+++ weboo-webapp/src/main/java/no/f12/jzx/weboo/server/WebServer.java
@@ -73,5 +73,33 @@ public class WebServer {
 			throw new RuntimeException(e);
 		}
 	}
+	
+	public static void main(String[] args) throws Exception {
+		int port = determineServerPort();
+		File contextPath = determineContextPath();
+
+		WebServer setupServer = new WebServer(port);
+		setupServer.start(contextPath, "");
+	}
+
+	private static File determineContextPath() {
+		File contextPath = new File("./src/main/webapp");
+		File herokuPath = new File("./weboo-webapp/src/main/webapp");
+
+		if (herokuPath.exists()) {
+			return herokuPath;
+		}
+		return contextPath;
+	}
+
+	private static int determineServerPort() {
+		int port = 8080;
+		String systemPort = System.getenv("PORT");
+		if (systemPort != null) {
+			port = Integer.valueOf(systemPort);
+		}
+		return port;
+	}
+
 
 }
